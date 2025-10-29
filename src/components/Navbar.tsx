9a35c8820236c321b58d18cc3e2062364e4d1ffa
Diego QR <diegoqr2000@gmail.com>
2025-10-29 11:13:46 -0400

    Add react-icons dependency and update Navbar component for hover effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import Logo from '../assets/logo.png';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { Observer } from 'gsap/Observer';
+import { IoIosArrowDown } from "react-icons/io";
+
 
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(Observer);
@@ -12,8 +14,8 @@ export default function Navbar() {
   useGSAP((_, contextSafe) => {
       // Animation for hover effects
       const hoverInAnimation = contextSafe!((e : HTMLElement) => {
-        gsap.set(e.getElementsByTagName('div'), {scaleY: 0, backgroundColor: '#FE7743', transformOrigin: 'bottom'});
-        gsap.to(e.getElementsByTagName('div'), 
+        gsap.set(e.getElementsByClassName('animated-box'), {scaleY: 0, backgroundColor: '#FE7743', transformOrigin: 'bottom'});
+        gsap.to(e.getElementsByClassName('animated-box'), 
         { 
           scaleY: 1, 
           duration: 0.5, 
@@ -21,7 +23,7 @@ export default function Navbar() {
         });
       });
       const hoverOutAnimation = contextSafe!((e : HTMLElement) => {
-        gsap.to(e.getElementsByTagName('div'), 
+        gsap.to(e.getElementsByClassName('animated-box'), 
         { 
           scaleY: 0, 
           duration: 0.5,
@@ -38,16 +40,23 @@ export default function Navbar() {
       <img src={Logo} alt="Logo" className='h-15'/>
       <ul className='flex items-stretch font-bold text-sm text-white'>
         <li className='hover-target flex items-center px-10 relative cursor-pointer'>
-            <a className='z-1'>INICIO</a>
-            <div className='w-full h-full absolute inset-x-0 bottom-0'></div>
+            <div className='z-1'>
+                <a>INICIO</a>
+            </div>
+            <div className='animated-box w-full h-full absolute inset-x-0 bottom-0'></div>
         </li>
         <li className='hover-target flex items-center px-10 relative cursor-pointer'>
-            <a className='z-1'>NUESTROS SERVICIOS</a>
-            <div className='w-full h-full absolute inset-x-0 bottom-0'></div>
+            <div className='z-1 inline-flex items-center gap-2'>
+                <a>NUESTROS SERVICIOS</a>
+                <IoIosArrowDown className='ml-1'/>
+            </div>
+            <div className='animated-box w-full h-full absolute inset-x-0 bottom-0'></div>
         </li>
         <li className='hover-target flex items-center px-10 relative cursor-pointer'>
-            <a className='z-1'>CONTACTO</a>
-            <div className='w-full h-full absolute inset-x-0 bottom-0'></div>
+            <div className='z-1'>
+                <a>CONTACTO</a>
+            </div>
+            <div className='animated-box w-full h-full absolute inset-x-0 bottom-0'></div>
         </li>
       </ul>
     </nav>
